e865e746c5c47da83dc0294a65296c330d75db5e
Damodar Lohani <dlohani48@gmail.com>
2023-01-17 12:39:26 +0000

    fix analytics

--- src/lib/components/Analytics.svelte
+++ src/lib/components/Analytics.svelte
@@ -14,14 +14,14 @@
 
 <svelte:head>
 	<!-- Google tag (gtag.js) -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id={PUBLIC_ANALYTICS_ID}"></script>
-	<script>
+	<script async src="https://www.googletagmanager.com/gtag/js?id={PUBLIC_ANALYTICS_ID}">
+	</script>
+	<script id="analytics" data-id={PUBLIC_ANALYTICS_ID}>
 		window.dataLayer = window.dataLayer || [];
 		function gtag() {
 			dataLayer.push(arguments);
 		}
 		gtag('js', new Date());
-
-		gtag('config', PUBLIC_ANALYTICS_ID);
+		gtag('config', document.currentScript.getAttribute('data-id'));
 	</script>
 </svelte:head>
