b6faf15eec4cc730ffdcf2ef96d25c77be7075d0
Damodar Lohani <dlohani48@gmail.com>
2023-04-28 12:15:04 +0545

    improvement and fix deployment error

---
+++ src/routes/(nested)/30days/flutter/footer.svelte
@@ -0,0 +1,22 @@
+<section>
+	<article>
+		<h2>#30DaysMasterFlutter</h2>
+		<p>
+			Learn the basics of Flutter and Dart in 30 days! We provide a roadmap, resources, and project
+			ideas to help you along your journey.
+		</p>
+		<a href="/30days/flutter">back to 30 Days to Master Flutter</a>
+	</article>
+</section>
+
+<style lang="scss">
+	section {
+		background-color: #fff;
+		padding: 2rem;
+		article {
+			max-width: 42rem;
+			margin: 0 auto;
+			text-align: center;
+		}
+	}
+</style>
