58488e29e9fb8d9309a746e47e8c9ff67e6a8abc
Damodar Lohani <dlohani48@gmail.com>
2023-04-28 19:22:05 +0545

    fix day release logic

--- src/routes/(nested)/30days/flutter/(days)/+layout.server.js
+++ src/routes/(nested)/30days/flutter/(days)/+layout.server.js
@@ -6,7 +6,7 @@ export async function load({ url, params, fetch, session, context }) {
     const paths = url.pathname.split('/');
     const day = parseInt(paths[paths.length - 1].replace('day', ''));
 
-    if(config.masterFlutter30Published.getTime() + day * 24 * 60 * 60 * 1000 > new Date().getTime()) {
+    if(config.masterFlutter30Published.getTime() + (day * 24 * 60 * 60 * 1000) > new Date().getTime()) {
         throw redirect(301, '/30days/flutter/coming-soon')
     }
 
--- src/routes/(nested)/30days/flutter/+layout.server.js
+++ src/routes/(nested)/30days/flutter/+layout.server.js
@@ -4,7 +4,7 @@ import { resources } from './resources';
 /** @type {import('./$types').PageLoad} */
 export async function load() {
     let index = (new Date().getTime() - config.masterFlutter30Published.getTime()) / (24 * 60 * 60 * 1000);
-    index = Math.round(index);
+    index = Math.floor(index);
     return {
         resources: resources.slice(0, index),
         daysReleased: index
