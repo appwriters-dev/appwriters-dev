6cff2770cc6f66c67ec61c09cb0d0ea7f161a949
Damodar Lohani <dlohani48@gmail.com>
2023-04-30 12:57:30 +0545

    fix config date

--- src/routes/(nested)/30days/config.js
+++ src/routes/(nested)/30days/config.js
@@ -1,3 +1,3 @@
 export const config = {
-    masterFlutter30Published: (new Date("2023-04-20")).getTime() - 6 * 60 * 60 * 1000, // to make it release at 6 AM Nepal time
+    masterFlutter30Published: (new Date("2023-04-20")).getTime(),
 }
