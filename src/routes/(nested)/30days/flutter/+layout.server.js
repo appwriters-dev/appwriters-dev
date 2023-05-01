8c6288687b115a72340f211c91f3a4a2942ab934
Damodar Lohani <dlohani48@gmail.com>
2023-05-01 07:24:20 +0545

    make new page every minute

--- src/routes/(nested)/30days/flutter/+layout.server.js
+++ src/routes/(nested)/30days/flutter/+layout.server.js
@@ -5,7 +5,7 @@ export const prerender = 'auto';
 
 /** @type {import('./$types').PageLoad} */
 export async function load() {
-    let index = (new Date().getTime() - config.masterFlutter30Published) / (24 * 60 * 60 * 1000);
+    let index = (new Date().getTime() - config.masterFlutter30Published) / ( 60 * 1000);
     index = Math.floor(index);
     return {
         resources: resources.slice(0, index),
