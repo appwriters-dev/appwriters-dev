48d37139340dfd6d85dc9b43c6f957dd29100c62
Damodar Lohani <dlohani48@gmail.com>
2023-05-01 07:27:51 +0545

    reduce 5:45 minutes

--- src/routes/(nested)/30days/config.js
+++ src/routes/(nested)/30days/config.js
@@ -1,3 +1,3 @@
 export const config = {
-    masterFlutter30Published: (new Date("2023-05-1 07:20")).getTime(),
+    masterFlutter30Published: (new Date("2023-05-1 07:20")).getTime() - (5 * 60 + 45) * 60 * 1000,
 }
