e483efdc0870e55708c26ce8156f7cd1b41becad
Damodar Lohani <dlohani48@gmail.com>
2023-05-05 14:54:40 +0545

    share to social media

--- src/routes/(nested)/30days/dayContent.svelte
+++ src/routes/(nested)/30days/dayContent.svelte
@@ -1,6 +1,10 @@
 <script>
+	import Share from '$lib/components/Share.svelte';
+	import { siteLink } from '$lib/config';
+
 	export let day = 0;
 	export let nextReleased = true;
+	export let description = '';
 </script>
 
 <header>
@@ -10,14 +14,17 @@
 
 <div class="container">
 	<slot />
+	<p>
+		<b>Enjoying? Tell your friends </b><Share message="🎉 I'm having a blast on day {day} of my #30DaysMasterFlutter journey! {description} 🚀 You should definitely start your own journey too! 💪" url="{siteLink}/30days/flutter/day{day}" />
+	</p>
 	<nav class="end-nav">
 		{#if day > 1}
-			<a href="/30days/flutter/day{day - 1}"><i class="icon-arrow-left" /> day {day -1}</a>
+			<a href="/30days/flutter/day{day - 1}"><i class="icon-arrow-left" /> day {day - 1}</a>
 		{:else}
 			<div />
 		{/if}
 		{#if nextReleased && day < 30}
-			<a href="/30days/flutter/day{day + 1}">day {day+1} <i class="icon-arrow-right" /> </a>
+			<a href="/30days/flutter/day{day + 1}">day {day + 1} <i class="icon-arrow-right" /> </a>
 		{/if}
 		{#if day == 30}
 			<a href="/30days/flutter/whats-next">What's Next? <i class="icon-arrow-right" /> </a>
@@ -43,7 +50,7 @@
 		position: relative;
 		box-sizing: border-box;
 		&::after {
-			content: "";
+			content: '';
 			position: absolute;
 			top: 0;
 			bottom: 0;
