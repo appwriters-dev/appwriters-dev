397739d9d4a060cf27eab4c8485d9b0aa3cad902
Damodar Lohani <dlohani48@gmail.com>
2023-05-05 15:27:44 +0545

    update to use env variables

--- src/routes/(nested)/30days/config.js
+++ src/routes/(nested)/30days/config.js
@@ -1,3 +1,5 @@
+import { PUBLIC_MF_30_PUBLISHED_DATE } from '$env/static/public';
+
 export const config = {
-    masterFlutter30Published: (new Date("2023-04-20")).getTime(),
+    masterFlutter30Published: (new Date(PUBLIC_MF_30_PUBLISHED_DATE)).getTime(),
 }
