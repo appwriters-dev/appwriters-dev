95c504d6b2dc3499a569428f44cb22fb5ead49e6
Damodar Lohani <dlohani48@gmail.com>
2023-05-24 10:04:33 +0545

    update

--- src/routes/(nested)/30days/flutter/testimonials.ts
+++ src/routes/(nested)/30days/flutter/testimonials.ts
@@ -2,7 +2,7 @@ export const testimonials = [
     {
         id: 1,
         name: 'Shankar Bir Tamang',
-        testimonial: 'While I was wondering how to begin in flutter development , this event has been very helpful for me to continue the journey. Though there is no live session , but the daily guidance and resource has become very useful for me . Salute for your continuous effort.',
+        testimonial: 'While I was wondering how to begin in Flutter development, this event has been very helpful for me to continue the journey. Though there is no live session, but the daily guidance and resource has become very useful for me. Salute for your continuous effort.',
         image: 'https://avatars.githubusercontent.com/u/85660085?v=4',
         github: 'https://github.com/ShankarBirTamang'
     }
