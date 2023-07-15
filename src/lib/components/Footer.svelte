c8df9e68a652a2dec1acb9417f69659a81546b8b
Damodar Lohani <dlohani48@gmail.com>
2023-07-15 06:16:57 +0545

    update footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -9,12 +9,6 @@
 	<nav>
 		<ul>
 			<li>
-				<a href="/flutter-apprenticeship">Flutter apprenticeship</a>
-			</li>
-			<li>
-				<a href="/30days/flutter">#30DaysMasterFlutter</a>
-			</li>
-			<li>
 				<a href="/api/rss.xml" data-sveltekit-reload>RSS</a>
 			</li>
 			<li>
@@ -22,7 +16,16 @@
 			</li>
 		</ul>
 	</nav>
-
+	<nav>
+		<ul>
+			<li>
+				<a href="/flutter-apprenticeship">Flutter apprenticeship</a>
+			</li>
+			<li>
+				<a href="/30days/flutter">#30DaysMasterFlutter</a>
+			</li>
+		</ul>
+	</nav>
 	<p>
 		&copy;{new Date().getFullYear()}
 		<a href={siteAuthorURL} target="_blank" rel="noreferrer">{siteAuthor}</a>
