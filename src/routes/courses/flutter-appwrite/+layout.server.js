dd9cfddf1fd3d33264f1e9e2d74e9c2d46bb1ecf
Damodar Lohani <dlohani48@gmail.com>
2024-01-21 17:55:53 +0545

    add metadata

--- src/routes/courses/flutter-appwrite/+layout.server.js
+++ src/routes/courses/flutter-appwrite/+layout.server.js
@@ -7,6 +7,8 @@ export const load = async ({ url, fetch }) => {
 	return {
         title: 'Complete Flutter and Appwrite course',
         subtitle: 'An exciting course to learn allabout building beautiful and functional cross-platform applications with Flutter using the backend power of Appwrite',
+        metaKeywords: 'flutter, appwrite, course, tutorial, learn, mobile, web, development, backend, api, database, storage, auth, authentication, security, cloud, open source',
+        metaDescription: 'An exciting course to learn allabout building beautiful and functional cross-platform applications with Flutter using the backend power of Appwrite',
         videoId: 'cmboLwHvsFU',
         curriculum: [
             {
